835e2968fda0806c5a921ac30d8deded1d86c5d2
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2021-11-06 15:17:33 +0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,56 @@
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit({
+  auth: process.env.GH_TOKEN,
+});
+
+var repos = [];
+
+octokit.rest.repos
+  .listForUser({
+    username: "tinvv",
+    sort: "full_name",
+    per_page: 100,
+  })
+  .then(({ data }) => {
+      data.forEach(async(repo, idx) => {
+        repos.push({ repo: repo.name, star: repo.stargazers_count, forks: repo.forks, issues: repo.open_issues, isFork: repo.fork, pulls: await (await octokit.rest.pulls.list({ owner: "tinvv", repo: repo.name })).data.length })
+        if(repos.length === data.length) {       
+          const readmeContent = await octokit.rest.repos.getReadme({
+            owner: "tinvv",
+            repo: "tinvv",
+          });
+            const content = Buffer.from(readmeContent.data.content, 'base64').toString();
+            const data = content.split('\n');
+
+            const start_line = data.indexOf("<!-- [PROFILE UPDATER]: START -->")
+            const end_line = data.indexOf("<!-- [PROFILE UPDATER]: END -->")
+
+            if(start_line === -1 && end_line === -1) {
+              console.log("Error: Could not find start/end line syntax")
+            }
+
+            data.splice(start_line + 1, end_line - start_line - 1, 
+              `## My Projects\n` +
+              `${repos.filter((repo) => !repo.isFork).map((repo) => `- [${repo.repo}](https://github.com/tinvv/${repo.repo})`+ (!repo.star && !repo.fork && !repo.pulls  && !repo.issues ? "" : " (" + (repo.star > 0 ? (` [${repo.star} stars](https://github.com/tinvv/${repo.repo}/stargazers)`) : ('')) + (" " + repo.pulls > 0 ? (` [${repo.pulls} pulls](https://github.com/tinvv/${repo.repo}/pulls)`) : ('')) + (" " + repo.fork > 0 ? (` [${repo.fork} forks](https://github.com/tinvv/${repo.repo}/network/members)`) : ('')) + (" " + repo.issues > 0 ? (` [${repo.issues} issues](https://github.com/tinvv/${repo.repo}/issues)`) : ('')) + " )")).sort().toString().split(',').join('\n')}` +
+              `\n\n` +
+              `## My contribution\n` +
+              `${repos.filter((repo) => repo.isFork).map((repo) => `- [${repo.repo}](https://github.com/tinvv/${repo.repo})`).sort().toString().split(',').join('\n')}`
+            )
+            const modify = data.join('\n');
+            octokit.rest.repos.createOrUpdateFileContents({
+              owner: "tinvv",
+              repo: "tinvv",
+              path: "README.md",
+              branch: "main",
+              message: "Update README.md",
+              sha: readmeContent.data.sha,
+              content: Buffer.from(modify).toString('base64'),
+              committer: {
+                name: "Tin's bot",
+                email: "Toddsbin@users.noreply.github.com"
+              }
+            });
+          }
+      });
+  });
